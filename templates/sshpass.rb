ba0196cf998cab6d1657f77da81331a7c79d1fd7
D Lio <d@derycka.com>
2022-01-04 20:18:23 +1100

    Init commit

---
+++ templates/sshpass.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Sshpass < Formula
+  url '$url'
+  homepage 'http://sourceforge.net/projects/sshpass'
+  sha256 '$sha256'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "sshpass"
+  end
+end
